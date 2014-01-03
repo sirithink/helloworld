0e1e81681bea483a70b082d11965b7dd556958fe
sirithink <sirithink@gmail.com>
2014-01-03 14:04:05 +0800

    init

---
+++ hello/src/Hello.java
@@ -0,0 +1,16 @@
+
+public class Hello {
+
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helloworld");
+	}
+
+}
